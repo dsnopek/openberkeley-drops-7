6a713985120fdbc2a446cda03bedc4e58c9936b0
Caroline Boyden <cboyden@gmail.com>
2014-08-11 12:55:19 -0700

    more whitespace fixes

--- profiles/openberkeley/rebuild.sh
+++ profiles/openberkeley/rebuild.sh
@@ -54,7 +54,7 @@ if [ -f openberkeley-prod.make ] || [ -f openberkeley-dev.make ]; then
     done
     drush -y make --no-core --contrib-destination=. openberkeley-dev.make
     cp modules/panopoly/panopoly_test/tests/features/*.feature openberkeley_tests/behat/features/panopoly/
-    cp modules/panopoly/panopoly_test/tests/features/bootstrap/PanopolyContext.php openberkeley_tests/behat/features/bootstrap/   
+    cp modules/panopoly/panopoly_test/tests/features/bootstrap/PanopolyContext.php openberkeley_tests/behat/features/bootstrap/
     for file in "openberkeley_tests/patches/*.patch"
     do
       patch -p3 < "$file"
